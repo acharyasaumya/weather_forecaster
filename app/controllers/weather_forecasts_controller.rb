a232b8ccb0bd58f7872694100e052546edbb800c
acharyasaumya <40291336+acharyasaumya@users.noreply.github.com>
2024-05-08 00:51:30 +0530

    add caching feature

--- app/controllers/weather_forecasts_controller.rb
+++ app/controllers/weather_forecasts_controller.rb
@@ -6,9 +6,9 @@ class WeatherForecastsController < ApplicationController
 
     def get_forecast
       zip_code = extract_zipcode(params[:address])
-      forecast = WeatherForecastService.new(zip_code).fetch_forecast
+      forecast, from_cache = cached_forecast(zip_code)
       tomorrow_forecast = WeatherForecastService.new(zip_code).fetch_tomorrow_forecast
-      render partial: 'layouts/weather_forecasts/forecast_result', locals: { forecast: forecast, tomorrow_forecast: tomorrow_forecast }, 
+      render partial: 'layouts/weather_forecasts/forecast_result', locals: { forecast: forecast, tomorrow_forecast: tomorrow_forecast, from_cache: from_cache  }, 
       layout: false
     end
 
@@ -19,6 +19,20 @@ class WeatherForecastsController < ApplicationController
       zip_code = address[-6..-1] || address[-5..-1]
     
       zip_code
+    end
+
+    def cached_forecast(zip_code)
+      cache_key = "weather_forecast_#{zip_code}"
+      forecast = Rails.cache.fetch(cache_key, expires_in: 30.minutes) do
+        fetch_and_cache_forecast(zip_code)
+      end
+      from_cache = forecast.present?
+      [forecast, from_cache]
+    end
+  
+    def fetch_and_cache_forecast(zip_code)
+      forecast = WeatherForecastService.new(zip_code).fetch_forecast
+      forecast.merge(from_cache: false)
     end
 end
   
